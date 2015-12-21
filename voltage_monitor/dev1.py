305dbd30da743e5923f7fd816da956410c9dde36
isseikz <issey.macky@gmail.com>
2015-12-21 12:52:28 +0900

    Update dev1
    recoded and tested successfully

--- voltage_monitor/dev1.py
+++ voltage_monitor/dev1.py
@@ -1,17 +1,17 @@
-#Beagle Bone Black用 電圧測定プログラム dev1
--*- coding: utf-8 -*-
-import time
 import Adafruit_BBIO.ADC as ADC
-f = open("data.dat","w")
-f.write("電圧測定プログラム")
+import time
 ADC.setup()
+f = open("test1.dat", "w")
+f.write("The Voltage Monitor")
 start = time.time()
-while true:
+while True:
   try:
-    voltage = ADC.read("P9_39")
+    voltage = ADC.read_raw("P9_40")
     elapsed_time = time.time() - start
-    f.write(elapsed_time,voltage)
+    f.write(str(voltage))
+    f.write(str(elapsed_time))
+    f.write('\n')
     print voltage
   except KeyboardInterrupt:
+    f.close()
     break
-f.close()
